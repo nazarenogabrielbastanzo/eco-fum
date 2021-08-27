e8094430b7fc38ee386282fd36b0b25481fa9868
Nazareno Gabriel Bastanzo <b.nazarenogabriel@gmail.com>
2021-08-27 07:29:30 -0300

    Go Up Button

--- assets/js/scripts1.js
+++ assets/js/scripts1.js
@@ -1,5 +1,26 @@
-let h2Elems = document.querySelectorAll('h2');
+// let h2Elems = document.querySelectorAll('h2');
 
-for (let h2Elem of h2Elems) {
-    h2Elem.textContent = (h2Elem.textContent).toUpperCase();
-}
+// for (let h2Elem of h2Elems) {
+//     h2Elem.textContent = (h2Elem.textContent).toUpperCase();
+// }
+
+$(function() {
+    let h2Elems = $('h2');
+    for (let h2Elem of h2Elems) {
+        h2Elem.textContent = (h2Elem.textContent).toUpperCase();
+    }
+
+    $('.go-up').click(function(){
+		$('body, html').animate({
+			scrollTop: '0px'
+		}, 300);
+	});
+
+	$(window).scroll(function(){
+		if( $(this).scrollTop() > 0 ){
+			$('.go-up').slideDown(300);
+		} else {
+			$('.go-up').slideUp(300);
+		}
+	});
+});
